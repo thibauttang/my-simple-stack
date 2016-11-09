5a252fdec4bedc40e07bd94d22620b6ac573199d
thibauttang <thibaut.tang@gmail.com>
2016-11-09 08:00:14 +0100

    First commit

---
+++ src/main/java/deqo/ttan/App.java
@@ -0,0 +1,13 @@
+package deqo.ttan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
